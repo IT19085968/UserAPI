b73153c401fce65b35e2156e0199329d964e92c6
IT19085968 <it19085968@my.sliit.lk>
2021-06-26 21:25:24 +0530

    initial commit

---
+++ UserAPI/web/src/main/java/com/userapi/web/UserController.java
@@ -0,0 +1,34 @@
+package com.userapi.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/userapi")
+@CrossOrigin
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/")
+    public void registerNewUser(@RequestBody User user) {
+        userService.addNewUser(user);
+    }
+}
