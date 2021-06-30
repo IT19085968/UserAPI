97eb7de48395d90c568f9f7d63a077547217900f
udithkav <67887799+udithkav@users.noreply.github.com>
2021-06-30 17:14:10 +0530

    Finished approve and decline changes.

--- UserAPI/web/src/main/java/com/userapi/web/AdminAPI/AdminService.java
+++ UserAPI/web/src/main/java/com/userapi/web/AdminAPI/AdminService.java
@@ -1,5 +1,6 @@
 package com.userapi.web.AdminAPI;
 
+import com.userapi.web.models.Conference;
 import com.userapi.web.models.EditorChanges;
 import com.userapi.web.repositories.ConferenceRepository;
 import com.userapi.web.repositories.EditorChangesRepository;
@@ -47,6 +48,27 @@ public class AdminService {
                 throw new IllegalStateException("Many Records Found");
             }else{
                 EditorChanges editorChange = conferenceEdit.get(0);
+                Query queryConference =new Query();
+                queryConference.addCriteria(Criteria.where("conferenceName").is(editorChanges.getConferenceName()));
+                List<Conference> changeConference = mongoTemplate.find(queryConference,Conference.class);
+
+
+
+                if(changeConference.size()>1){
+                    throw new IllegalStateException("Many Records Found");
+                }
+                else{
+                    Conference conferenceFinal = changeConference.get(0);
+                    conferenceFinal.setConferenceName(editorChanges.getConferenceName());
+                    conferenceFinal.setDate(editorChanges.getDate());
+                    conferenceFinal.setVenue(editorChanges.getVenue());
+                    conferenceFinal.setStartTime(editorChanges.getStartTime());
+                    conferenceFinal.setDuration(editorChanges.getDuration());
+                    conferenceFinal.setNumberOfAttendees(editorChanges.getNumberOfAttendees());
+                    conferenceFinal.setMaximumAttendees(editorChanges.getMaximumAttendees());
+                    conferenceRepository.save(conferenceFinal);
+                    editorChangesRepository.deleteById(editorChanges.getId());
+                }
 
             }
         }
